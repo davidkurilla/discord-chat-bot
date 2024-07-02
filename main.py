9209ff382da642efdb46bf4593bd4b58e0f06901
David Kurilla <13390793-DavidKurilla1@users.noreply.replit.com>
2024-07-02 20:12:18 +0000

    feat: create Discord bot

--- main.py
+++ main.py
@@ -1,24 +1,54 @@
 import os
+import discord
 from groq import Groq
+
+
+intents = discord.Intents.all()
+bot = discord.Client(intents=intents)
+discord_key = os.environ['DISCORD_KEY']
 
 client = Groq(
   api_key = os.environ['LLAMA_KEY'],
 )
 
-prompt = input("Enter a prompt: ")
-
-while prompt != "quit":
+async def callGroq(prompt):
   
-  chat_completion = client.chat.completions.create(
-    messages = [
-      {
-        "role": "user",
-        "content": f"{prompt}",
-      }
-    ],
-    model = "llama3-8b-8192",
-  )
+  try:
 
-  print(chat_completion.choices[0].message.content)
+    print(f"Prompt: {prompt}")
+  
+    chat_completion = client.chat.completions.create(
+      messages = [
+        {
+          "role": "user",
+          "content": f"Respondin under 2000 characters in slighty rude UwU speech: {prompt}",
+        }
+      ],
+      model = "llama3-8b-8192",
+    )
+  
+    return chat_completion.choices[0].message.content
 
-  prompt = input("Enter a prompt: ")
+  except Exception as e:
+    print(e)
+    return "Error"
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+  try: 
+    if message.author == bot.user or bot.user not in message.mentions:
+      return
+    
+    else:
+      response = await callGroq(message.content)
+      await message.channel.send(response)
+      
+  except Exception as e:
+    print(e)
+    await message.channel.send("Error")
+
+bot.run(discord_key)
